240b34321b8bb555610b20ee5814803170fd85ca
tclesius <tomclesius@gmail.com>
2022-12-06 14:27:16 +0100

    aoc3 finished !!

--- 2022/03/aoc3.java
+++ 2022/03/aoc3.java
@@ -4,30 +4,70 @@ import java.util.*;
 class aoc3{
     static String encoding = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
+
+    static boolean all(boolean [] array){
+        for(boolean b : array) if(!b) return false;
+        return true;
+    }
+
+
+    static String commonChars(String... groups){
+        String commonChars = "";
+
+
+        for (char c : encoding.toCharArray()){
+            boolean commonCharTest[] = new boolean[groups.length];
+            for (int i = 0; i < groups.length; i++){
+                commonCharTest[i] = groups[i].indexOf(c) < 0 ? false : true;
+            }
+
+            if (all(commonCharTest)) commonChars += c;
+        }
+        return commonChars;
+    }
+
+
+
     static int solution1() throws Exception{
         File file = new File("D:\\dev\\adventofcode\\2022\\03\\input.txt");
         Scanner sc = new Scanner(file);
         int sum = 0;
         while(sc.hasNextLine()){    
-            char[] line = sc.nextLine().toCharArray();
-            ArrayList<Character> lhs = new ArrayList<Character>();
-            ArrayList<Character> rhs = new ArrayList<Character>();
-
-
-            for(int i = 0; i<line.length/2; i++){
-                lhs.add(line[i]);
+            String s = sc.nextLine();
+            String common = commonChars(s.substring(0,s.length()/2),s.substring(s.length()/2,s.length()));
+            for(char c : common.toCharArray()){
+                sum += encoding.indexOf(c);
             }
-            for(int i = line.length/2; i<line.length; i++){
-                rhs.add(line[i]);
-            }           
-
-            lhs.retainAll(rhs);
-            sum += encoding.indexOf(lhs.get(0));
         }
+        return sum;
+    }
+
+    static int solution2()throws Exception{
+        File file = new File("D:\\dev\\adventofcode\\2022\\03\\input.txt");
+        Scanner sc = new Scanner(file);
+        Set<Character> commonChars = new LinkedHashSet<>();
+        int sum = 0;
+        while(sc.hasNextLine()){    
+            String lines = "";
+            
+            for(int i = 0; i < 3; i++){
+                lines += sc.nextLine() + " ";
+            }
+
+            String [] linesArr = lines.split(" ");
+            
+            String common = commonChars(linesArr);
+            for(char c : common.toCharArray()){
+                sum += encoding.indexOf(c);
+            }
+        }
+
+
         return sum;
     }
     
     public static void main(String[] args)throws Exception{
         System.out.println(solution1());
+        System.out.println(solution2());
     }
 }
